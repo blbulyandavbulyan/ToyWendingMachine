e8e592112237c68471b58f385d6a113f0f8d99ac
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-08-30 18:23:28 +0400

    added reading user input, now program asks user

--- src/main/java/org/blbulyandavbulyan/Main.java
+++ src/main/java/org/blbulyandavbulyan/Main.java
@@ -5,6 +5,7 @@ import com.opencsv.CSVReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,7 +23,6 @@ public class Main {
             }
             var toyVendingMachine = new ToyVendingMachine();
             CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(toyFileName)));
-
             for (String[] line : csvReader) {
                 if (line.length != 3)
                     throw new RuntimeException("Количество столбцов в csv файле должно быть 3!");
@@ -31,6 +31,15 @@ public class Main {
                 double dropFrequency = Double.parseDouble(line[2]);
                 toyVendingMachine.addToy(title, amount, dropFrequency);
             }
+            Scanner scanner = new Scanner(System.in);
+            while (toyVendingMachine.hasToy()){
+                System.out.print("Хотите игрушку ?(yes/no): ");
+                if(scanner.nextLine().equalsIgnoreCase("yes"))
+                    System.out.println(toyVendingMachine.getRandomToy());
+                else break;
+            }
+            if(!toyVendingMachine.hasToy())
+                System.out.println("Игрушки закончились :(");
         } catch (FileNotFoundException | RuntimeException e) {
             System.out.println(e.getMessage());
         }
